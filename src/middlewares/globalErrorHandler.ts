3422a28d577dfffbf678e87dae841110ed36298e
Imran Hasan <imranhasanrimon5@gmail.com>
2025-07-20 19:05:53 +0600

    entire project is done

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,18 +1,34 @@
 import { ErrorRequestHandler } from "express";
 import mongoose from "mongoose";
 
-export const globalErrorHandler: ErrorRequestHandler = (err, req, res) => {
+export const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     let statusCode = 500;
     let message = "Something went wrong";
 
+    // Handle Mongoose Validation Error
     if (err instanceof mongoose.Error.ValidationError) {
         statusCode = 400;
         message = "Validation failed";
     }
 
+    // Handle Mongoose Cast Error (like invalid ObjectId)
+    else if (err instanceof mongoose.Error.CastError) {
+        statusCode = 400;
+        message = "Invalid ID format";
+    }
+
+    // Handle general JS Error
+    else if (err instanceof Error) {
+        message = err.message;
+    }
+
     res.status(statusCode).json({
-        message,
         success: false,
-        error: err,
+        message,
+        error: {
+            name: err.name,
+            message: err.message,
+            stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+        },
     });
 };
